74e372ff1ac800d61b13e74779bc152239b387e4
ninaaa12334 <160941480+ninaaa12334@users.noreply.github.com>
2025-01-28 17:04:29 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,14 +1,11 @@
 <?php
-// Database configuration
-$host = 'localhost'; 
-$db_name = 'cv'; 
-$username = 'root'; 
-$password = ''; 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test1";
 
-// Create connection
-$conn = new mysqli($host, $username, $password, $db_name);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
